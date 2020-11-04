93553f4b5fdc8f4611382ace2bc839fd2c45b56f
ppvalluri09 <ppvalluri09@outlook.com>
2020-11-04 19:34:29 +0530

    Fixed errors for scripts

--- run_target_sas.sh
+++ run_target_sas.sh
@@ -4,7 +4,7 @@ n=1
 while read line; do
   if [ $n -ge 2 ]
   then
-    python3 ./SMILES-LSTM/mol_dqn/chemgraph/target_sas.py --ouput_dir="./save" --hparams="./SMILES-LSTM/mol_dqn/chemgraph/target_sas.json"--start_molecule="$line" --loss_type="l2" --target_sas=2.5
+    python3 ./SMILES-LSTM/mol_dqn/chemgraph/target_sas.py --model_dir="./save" --hparams="./SMILES-LSTM/mol_dqn/chemgraph/configs/target_sas.json" --start_molecule="$line" --loss_type="l2" --target_sas=2.5
   fi
   n=$((n+1))
 done < $filename
