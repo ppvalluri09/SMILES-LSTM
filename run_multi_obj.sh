6a2ab9a9718320dfb68055e27eeb6d85e9fc42a8
ppvalluri09 <ppvalluri09@outlook.com>
2020-11-05 16:27:49 +0530

    Added script for training and generating SMILES. Also added installer script to make things easy

--- run_multi_obj.sh
+++ run_multi_obj.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-filename='./SMILES-LSTM/temp/smiles_cleansed'
+filename='./temp/smiles_cleansed'
 n=1
 while read line; do
   if [ $n -eq 1 ] 
   then
     cid=$line
   else
-    python ./SMILES-LSTM/mol_dqn/chemgraph/multi_obj_opt.py --model_dir="./save" --hparams="./SMILES-LSTM/mol_dqn/chemgraph/configs/multi_obj_dqn.json" --start_molecule="$cid" --target_molecule="$line" --similarity_weight=0.0 
+    python ./mol_dqn/chemgraph/multi_obj_opt.py --model_dir="./save" --hparams="./mol_dqn/chemgraph/configs/multi_obj_dqn.json" --start_molecule="$cid" --target_molecule="$line" --similarity_weight=0.0 
   fi
   n=$((n+1))
 done < $filename
