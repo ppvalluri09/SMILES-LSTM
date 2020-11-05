5e726795e2fdc9681591975e690cebbb18a04a39
ppvalluri09 <ppvalluri09@outlook.com>
2020-11-05 09:18:36 +0530

    Extracted SMILES extended, generative model trainde

--- modules/cid_to_smiles.py
+++ modules/cid_to_smiles.py
@@ -3,8 +3,9 @@ import pandas as pd
 import pubchempy as pc
 from tqdm import tqdm
 from collections import OrderedDict
+import os
 
-def get_smiles(pdbids, cid=None):
+def get_smiles(pdbids, cid=None, limit=-1):
     connection = sqlite3.connect("../../automated_in_silico_identification/build2/DB/drugs.db")
     df = pd.read_sql('SELECT * from drugs_interaction_processed;', connection)
     df = df[df["pbdid"].isin(pdbids)]
@@ -12,7 +13,12 @@ def get_smiles(pdbids, cid=None):
     if cid is not None:
         cids += [cid]
     cids += df["cid"].unique().tolist()
+    if limit == -1:
+        limit = len(cids) + 1
     print("Converting,", len(cids), "CIDs, to SMILES format")
+    if os.path.isfile("../temp/extracted_smiles"):
+        print("File found, replacing existing file")
+        os.system("rm ../temp/extracted_smiles")
     with open("../temp/extracted_smiles", "w") as f:
-        for cid in tqdm(cids[:10], total=len(cids[:10]), leave=True):
+        for cid in tqdm(cids[:limit], total=len(cids[:limit]), leave=True):
             f.write(pc.Compound.from_cid(cid).canonical_smiles + "\n")
--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
+if [ ! -f ./temp/smiles_cleansed.smi ]; then
+  python3 "./LSTM_Chem/cleanup_smiles.py" "./temp/extracted_smiles" "./temp/smiles_cleansed.smi"
+else
+  echo "File found, replacing old file"
+  rm ./temp/smiles_cleansed.smi
+  python3 "./LSTM_Chem/cleanup_smiles.py" "./temp/extracted_smiles" "./temp/smiles_cleansed.smi"
+fi
 
-python3 "./LSTM_Chem/cleanup_smiles.py" "./temp/extracted_smiles" "./temp/smiles_cleansed"
+cd LSTM_Chem
+python3 train.py
